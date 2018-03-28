25c3c32f2e636c1cc495d4aa230c02f32c6bc10d
wahahab <wahahab11@gmail.com>
2018-03-28 16:24:53 +0800

    finish ps0

---
+++ ps0/ps0.py
@@ -0,0 +1,7 @@
+import math
+
+if __name__ == '__main__': 
+    x = int(input('Enter number x: '))
+    y = int(input('Enter number y: '))
+    print 'x**y =', x**y
+    print 'log(x) =', math.log(x, 2)
